bb67f41a64e19103cb2bd99202e4faf1ad2d2637
rohannegi2507 <rohan.negi.dev@gmail.com>
2020-09-01 01:24:20 +0530

    feat(setup): intial project setup

---
+++ myaashiyana/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">MyAashiyana</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#" disabled>Feature</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+export default class Home extends Vue {}
+</script>
